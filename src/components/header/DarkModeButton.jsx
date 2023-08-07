1b97544a52f3e183310972bbd9b0d70a7adc355e
wreathmoon <luohao.xu22@imperial.ac.uk>
2023-08-07 09:24:28 +0800

    try new button

--- src/components/header/DarkModeButton.jsx
+++ src/components/header/DarkModeButton.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 
-import DarkModeToggle from "react-dark-mode-toggle";
+import { DarkModeToggle } from "react-dark-mode-toggle-2";
 
 function DarkModeButton(){
     const [isDarkMode, setIsDarkMode] = useState(true);
 
     document.querySelector('body').setAttribute("data-theme", isDarkMode===true?'dark':'light');
 
-    function switchTheme(){
+    function toggleDarkMode(){
         isDarkMode === true ? setIsDarkMode(false) : setIsDarkMode(true);
         document.querySelector('body').setAttribute("data-theme", isDarkMode===true?'dark':'light');
-    }
+    };
 
     return (
-        <DarkModeToggle checked={isDarkMode} onChange={setIsDarkMode} size={80} speed={2} />
+        <DarkModeToggle isDarkMode={isDarkMode} onChange={toggleDarkMode} size={80} speed={2} />
   )
 }
 
