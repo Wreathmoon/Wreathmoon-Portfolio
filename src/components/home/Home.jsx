e7f956767196303cc4dff9ea65e54b8bea83d74b
Luohao Xu <88706083+Wreathmoon@users.noreply.github.com>
2024-07-23 20:59:14 +0100

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
             <div className='home_container'>
                 <h5>Hello I'm</h5>
                 <h1>Wreathmoon</h1>
-                <h5 className='text-light'>Frontend Developer</h5>
+                
                 <CVButton />
             </div>
         </section>
